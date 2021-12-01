856b82644fc25963eb0b08eada116f150f110d7d
Henrik Alsér <henrik.alser@me.com>
2021-12-01 10:15:35 +0100

    Day 1

--- src/bin/1b.rs
+++ src/bin/1b.rs
@@ -1,10 +1,10 @@
-use std::io::{BufRead, BufReader, Error, ErrorKind::InvalidData};
+use std::io::{BufRead, BufReader, Error};
 use std::{collections::VecDeque, fs::File};
 
 fn main() -> Result<(), Error> {
     let entries = BufReader::new(File::open("./input/1.txt")?)
         .lines()
-        .map(|l| l.and_then(|v| v.parse().map_err(|e| Error::new(InvalidData, e))))
+        .map(|l| l.map(|v| v.parse().unwrap()))
         .collect::<Result<Vec<u32>, _>>()?;
     let (n, _) = entries.iter().fold(
         (0_u32, VecDeque::with_capacity(4)),
