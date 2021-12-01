c48daa38f56a46b7431dd13b780e64458777b094
Henrik Alsér <henrik.alser@me.com>
2021-12-01 09:15:01 +0100

    Day 1a

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -7,12 +7,11 @@ fn main() -> Result<(), Error> {
         .map(|l| l.and_then(|v| v.parse().map_err(|e| Error::new(InvalidData, e))))
         .collect::<Result<Vec<u32>, _>>()?;
     let (n, _) = entries.iter().fold((0_u32, None), |(n, prev), &curr| {
-        match prev.map(|p: u32| curr > p) {
+        match prev.map(|p| curr > p) {
             Some(true) => (n + 1, Some(curr)),
             _ => (n, Some(curr)),
         }
     });
-
     println!("{}", n);
     Ok(())
 }
