d090a5d3daaf34d13dbc5b1648bc35bda17004d1
Henrik Alsér <henrik.alser@me.com>
2021-12-08 00:21:35 +0100

    Day 7b

--- src/bin/7b.rs
+++ src/bin/7b.rs
@@ -2,8 +2,8 @@ use std::{fs::read_to_string, io::Error};
 
 fn main() -> Result<(), Error> {
     let input = read_to_string("./input/7.txt")?;
-    let crabs: Vec<i64> = input.split(',').map(|x| x.parse().unwrap()).collect();
-    let fuel: Option<i64> = crabs
+    let crabs: Vec<i32> = input.split(',').map(|x| x.parse().unwrap()).collect();
+    let fuel: Option<i32> = crabs
         .iter()
         .map(|&x0| {
             crabs
